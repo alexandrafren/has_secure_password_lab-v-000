2e41da30aff86f096875c98812bbf14ce7b1628e
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-02 19:39:42 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
     redirect_to '/home'
   end
 
+  def home
+  end
+
 private
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation, :name)

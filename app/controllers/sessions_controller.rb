830e663fc1975c4bfdb2904ed46c6e3863662132
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-02 19:46:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,6 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect_to '/home'
   end
 end

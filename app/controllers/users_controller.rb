b3644ba01214f503d56dbbab445377992a93b0f9
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-02 19:31:39 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    User.create(user_params)
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 end
